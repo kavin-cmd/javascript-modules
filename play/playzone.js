ed7c2cc4cf39477b3287d0c4e1f19ac409585485
Kavin Parikh <kpk1611@gmail.com>
2025-03-07 17:13:42 +0530

    timer.js project in play folder

--- play/playzone.js
+++
@@ -1,9 +0,0 @@
-function factorial(n) {
-     if (n === 0 || n === 1) return 1;
-     return n * factorial(n - 1);
- }
- console.log(factorial(5));
-
-//  Task
-//  Create a function that calculates the area of a 
-// rectangle.
